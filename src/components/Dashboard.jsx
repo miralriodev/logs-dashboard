2ac1c83a72e1bffa5e74e24226b5f10589b1f168
Hugo Miralrio <miralriohugo7@gmail.com>
2025-04-13 00:10:20 -0600

    fixing the cerrar sesion button in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -808,7 +808,8 @@ const Dashboard = () => {
               <button
                 onClick={() => {
                   localStorage.removeItem('isAuthenticated');
-                  window.location.reload();
+                  window.location.href = '/';
+
                 }}
                 className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
               >
